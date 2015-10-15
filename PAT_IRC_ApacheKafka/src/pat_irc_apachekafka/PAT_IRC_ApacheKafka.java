09128f394b641f987140238bf1129308245750df
AndariasSilvanus <asilvanus5@gmail.com>
2015-10-15 16:10:01 +0700

    first commit

---
+++ PAT_IRC_ApacheKafka/src/pat_irc_apachekafka/PAT_IRC_ApacheKafka.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pat_irc_apachekafka;
+
+/**
+ *
+ * @author Andarias Silvanus & Rita Sarah
+ */
+public class PAT_IRC_ApacheKafka {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
